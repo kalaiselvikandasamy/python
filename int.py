f9885795b0047ba4d4442a337aa4be9b3b9ab025
kalaiselvikandasamy <53572817+kalaiselvikandasamy@users.noreply.github.com>
2019-08-02 14:28:43 +0530

    Update int.py

--- int.py
+++ int.py
@@ -1,10 +1,8 @@
 n=int(input("enter an integer:")
 if(n>0):
   print("it is positive number")
-elif(n<0):  print("it is positive number")
-
+elif(n<0):
   print("it is negative number")
 else:
   print("it is zero")
-  print("it is positive number")
-  print("it is positive number")
+ee5fc8617789d4a4e2e3c0b2c2b3ecdec695ee2f

77d9648748fd4b4a4309feabf379c532e7eef89e
kalaiselvikandasamy <53572817+kalaiselvikandasamy@users.noreply.github.com>
2019-08-05 00:44:15 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,10 @@
+def name(fullname):
+  print("name is:"," ")
+def age(in yrs):
+  print("age is:"," ")
+def address(city):
+  print("address is:"," ")
+def name(aishukandan)
+def age(19)
+def(raja nagar,kallakurichi)
+  

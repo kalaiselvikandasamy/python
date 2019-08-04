c08f55d1da0aacb75aaab00959826d865ec81c0a
kalaiselvikandasamy <53572817+kalaiselvikandasamy@users.noreply.github.com>
2019-08-04 22:12:34 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number:")
+b=int(input("enter the second number:")
+c=int(input("enter the third number:")
+z=a+b+c
+print("the sum is:",z)
+if(a==b==c)
+  M=3(z)
+print("the value is:",M)

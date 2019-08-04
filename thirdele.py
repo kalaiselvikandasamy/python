d2d13a464eca3a3441ee35368f1a66dfc27165a3
kalaiselvikandasamy <53572817+kalaiselvikandasamy@users.noreply.github.com>
2019-08-05 01:11:27 +0530

    Create thirdele.py

---
+++ thirdele.py
@@ -0,0 +1,12 @@
+def remove(list):
+  position=3-1
+  index=0
+  len_list(len(list))
+  while len_list>0:
+    index=(position+indx)%len_list
+    print(int_list.pop(index))
+    len_list-=1
+nums=[10,20,30,40,50,60,70,80,90]
+remove(nums)
+    
+  

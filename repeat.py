cdcc1dccf82e288f044888d2f6e6784a11e052a2
kalaiselvikandasamy <53572817+kalaiselvikandasamy@users.noreply.github.com>
2019-08-05 14:19:09 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,3 +1,6 @@
-tuple=(2,6,7,2,9,4,2)
-count=tuple.count(2)
-print(count)
+tuplex = 2, 4, 5, 6, 2, 3, 4, 4, 7
+
+rep = {x for x in tuplex if tuplex.count(x) > 1}
+print('Repeated items in tuplex: ', rep)
+
+ 
